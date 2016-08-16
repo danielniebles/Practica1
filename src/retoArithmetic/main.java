0574b7f662c4a8cce558c6b5041deb5ea4d0bdce
tatiana-cespedes <alejandra.cespedes@udea.edu.co>
2016-08-15 20:22:53 -0500

    Versión 1.0

--- src/retoArithmetic/main.java
+++ src/retoArithmetic/main.java
@@ -11,15 +11,30 @@ import java.util.Scanner;
  * @author Daniel
  */
 public class main {
+    
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         String cadena;
+        char[] cifras1 = new char[3];
+        char[] cifras2 = new char[3];
+        arithmetic arithmetic1 = new arithmetic();
         System.out.print("Ingrese la cadena de prueba: ");
         cadena = lector.nextLine();
         String[] partes = cadena.split(" ");
-        int[] lista = new int[partes.length];
-        for(int i=0;i<partes.length;i++){
-            lista[i] = Integer.parseInt(partes[i]);
-        }  
+        int[] b = new int[(partes.length-2)/2];
+        int j = 0;
+       
+        for(int i=0;i<partes.length-2;i+=2){
+            cifras1 = partes[i].toCharArray();
+            if(i<partes.length-2){
+            cifras2 = partes[i+1].toCharArray();
+            }
+            b[j] = arithmetic1.carry(cifras1, cifras2);
+            j++;
+        }
+        arithmetic1.mostrar(b);
+        
     }
+
 }
+//lista[i] = Integer.parseInt(partes[i]);
